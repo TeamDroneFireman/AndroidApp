b7e4df2577fb2765020ec6065764f1efbabf06ce
LaurelineGuerin <laurelineguerin@laposte.net>
2016-04-20 15:15:58 +0200

    add: IAction

--- app/src/main/java/edu/istic/tdf/dfclient/domain/element/IAction.java
+++ app/src/main/java/edu/istic/tdf/dfclient/domain/element/IAction.java
@@ -4,4 +4,10 @@ package edu.istic.tdf.dfclient.domain.element;
  * Created by btessiau on 20/04/16.
  */
 public interface IAction extends IElement {
+
+    /**
+     * Change the action'state
+     * @param state
+     */
+    public void setState(ActionState state);
 }
