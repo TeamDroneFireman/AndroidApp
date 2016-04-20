f5c71c88d4229edbef56973c6bb394630f0ee021
baptiste_tessiau <baptiste.tessiau@gmail.com>
2016-04-20 14:52:26 +0200

    Start implements  Intervention and Sinister

---
+++ app/src/main/java/edu/istic/tdf/dfclient/domain/Sinister.java
@@ -0,0 +1,73 @@
+package edu.istic.tdf.dfclient.domain;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+import edu.istic.tdf.dfclient.domain.element.mean.IMean;
+
+/**
+ * Created by btessiau on 20/04/16.
+ */
+public class Sinister implements ISinister {
+
+    /**
+     * the sinisterCode of the sinister
+     */
+    private String sinisterCode;
+
+    /**
+     * the default collection of means for the defined sinisterCode
+     */
+    private Collection<IMean> means;
+
+    public Sinister() {
+        init();
+    }
+
+    // Initialize attributs
+    private void init(){
+        this.means = new ArrayList<IMean>();
+
+    }
+
+    @Override
+    public String getSinisterCode() {
+        return this.sinisterCode;
+    }
+
+    @Override
+    public void setSinisterCode(String sinisterCode) {
+        this.sinisterCode = sinisterCode;
+    }
+
+    @Override
+    public boolean addMean(IMean mean) {
+        if (this.means != null && mean != null)
+        {
+            return this.means.add(mean);
+        }
+
+        return false;
+    }
+
+    @Override
+    public boolean removeMean(IMean mean) {
+        if (this.means != null && mean != null)
+        {
+            return this.means.remove(mean);
+        }
+
+        return false;
+    }
+
+    @Override
+    public Iterator<IMean> getIteratorOnMeans() {
+        if (means != null)
+        {
+            return this.means.iterator();
+        }
+
+        return null;
+    }
+}
