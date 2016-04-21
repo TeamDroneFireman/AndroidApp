c901fdaf9927e2f267bbaeced7abca36d23ab950
AlexandreMumble <alexandremazure@gmail.com>
2016-04-21 12:05:04 +0200

    Ajout de Sitac fragment + Layout

---
+++ app/src/main/java/edu/istic/tdf/dfclient/fragment/ContextualDrawerFragment.java
@@ -0,0 +1,59 @@
+package edu.istic.tdf.dfclient.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import edu.istic.tdf.dfclient.R;
+
+public class ContextualDrawerFragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    public ContextualDrawerFragment() {
+        // Required empty public constructor
+    }
+
+    public static ContextualDrawerFragment newInstance() {
+        ContextualDrawerFragment fragment = new ContextualDrawerFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_contextual_drawer, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+    }
+}
---
+++ app/src/main/java/edu/istic/tdf/dfclient/fragment/ToolbarFragment.java
@@ -0,0 +1,59 @@
+package edu.istic.tdf.dfclient.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import edu.istic.tdf.dfclient.R;
+
+public class ToolbarFragment extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    public ToolbarFragment() {
+        // Required empty public constructor
+    }
+
+    public static ToolbarFragment newInstance() {
+        ToolbarFragment fragment = new ToolbarFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_toolbar, container, false);
+
+        return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+    }
+}
