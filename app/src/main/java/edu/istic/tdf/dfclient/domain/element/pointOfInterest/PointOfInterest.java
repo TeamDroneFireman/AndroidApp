73c19e3ee4921a8082e403d619a7c879b8e69029
Maxime Lebastard <lebastardmaxime@gmail.com>
2016-04-29 09:14:09 +0200

    Remove fields duplicates on PointOfInterest

--- app/src/main/java/edu/istic/tdf/dfclient/domain/element/pointOfInterest/PointOfInterest.java
+++ app/src/main/java/edu/istic/tdf/dfclient/domain/element/pointOfInterest/PointOfInterest.java
@@ -14,55 +14,6 @@ import edu.istic.tdf.dfclient.drawable.PictoFactory;
  */
 public class PointOfInterest extends Element implements IPointOfInterest {
 
-    /**
-     * The role of this element
-     */
-    private Role role;
-
-    /**
-     * The name of this element
-     * //TODO check if we used the name for identify water point or other
-     */
-    private String name;
-
-    /**
-     * The location of this element
-     */
-    private Location location;
-
-    public PointOfInterest() {
-    }
-
-    @Override
-    public void setRole(Role role) {
-        this.role=role;
-    }
-
-    @Override
-    public Role getRole() {
-        return this.role;
-    }
-
-    @Override
-    public void setLocation(Location location) {
-        this.location=location;
-    }
-
-    @Override
-    public Location getLocation() {
-        return this.location;
-    }
-
-    @Override
-    public void setName(String name) {
-        this.name=name;
-    }
-
-    @Override
-    public String getName() {
-        return this.name;
-    }
-
     @Override
     public ElementType getType() {
         return ElementType.POINT_OF_INTEREST;
