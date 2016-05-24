338d22680cd149da78c22d0e56a4ec88c1d9d0e9
alex_tremoceiro <alextremoceiro@gmail.com>
2016-05-24 14:50:56 +0200

    Adding sinister and means at creation

--- app/src/main/java/edu/istic/tdf/dfclient/domain/sinister/Sinister.java
+++ app/src/main/java/edu/istic/tdf/dfclient/domain/sinister/Sinister.java
@@ -1,20 +1,30 @@
 package edu.istic.tdf.dfclient.domain.sinister;
 
+import com.raizlabs.android.dbflow.annotation.Table;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import edu.istic.tdf.dfclient.database.TdfDatabase;
 import edu.istic.tdf.dfclient.domain.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 /**
  * Created by tremo on 27/04/16.
  */
+@Table(database = TdfDatabase.class)
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Sinister extends Entity implements ISinister{
     @Getter
     @Setter
-    private int code;
+    private String code;
 
     @Getter
     @Setter
