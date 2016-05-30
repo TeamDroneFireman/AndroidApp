cea520e83018438e82eb26bfc2a24e0504ad8870
baptiste_tessiau <baptiste.tessiau@gmail.com>
2016-05-30 13:49:52 +0200

    add: start gallery

--- app/src/main/java/edu/istic/tdf/dfclient/http/TdfHttpClient.java
+++ app/src/main/java/edu/istic/tdf/dfclient/http/TdfHttpClient.java
@@ -16,7 +16,7 @@ public class TdfHttpClient {
 
     private final static String SCHEME = "http";
     //private final static String HOST = "projetm2gla.istic.univ-rennes1.fr";
-    private final static String HOST = "devprojetm2gla.istic.univ-rennes1.fr";
+    private final static String HOST = "projetm2gla.istic.univ-rennes1.fr";
 
     public static final String HTTP_ACCEPT = "application/json";
     public static final String HTTP_CONTENT_TYPE = "application/json; charset=utf-8";
