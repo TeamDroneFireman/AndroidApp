91a238a6f0d09b8a73b91cbd162f9b57138fca4b
baptiste_tessiau <baptiste.tessiau@gmail.com>
2016-06-02 08:52:52 +0200

    fix: index out of bound when validate and refresh at high frequence

--- app/src/main/java/edu/istic/tdf/dfclient/UI/adapter/InterventionListAdapter.java
+++ app/src/main/java/edu/istic/tdf/dfclient/UI/adapter/InterventionListAdapter.java
@@ -1,14 +1,10 @@
 package edu.istic.tdf.dfclient.UI.adapter;
 
-import android.app.Application;
 import android.content.Context;
-import android.graphics.Color;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -63,7 +59,8 @@ public class InterventionListAdapter extends ArrayAdapter<String> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
-        if(interventions.get(position)!=null) {
+        if(interventions.size() > position && //avoid index out of bound exception when loading interventions
+                interventions.get(position)!=null) {
 
             View rowView = inflater.inflate(R.layout.intervention_row, null, true);
 
